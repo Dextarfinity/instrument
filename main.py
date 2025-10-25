3329ce20256ec359f1801b38b9adc4cb117332fb
dextarfinity <xdfeverharsh@gmail.com>
2025-10-25 11:13:22 +0800

    Remove cv2 import and opencv dependency - not needed for cloud deployment

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import uvicorn
-import cv2
 import numpy as np
 from ultralytics import YOLO
 import io
